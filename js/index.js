8a4a5eea28e455c0e9f86370ef66f7fc53ce94db
jonathan bola <bolajonathan@yahoo.com>
2022-02-03 14:08:47 -0600

    commit tree c188f98367acf9dd864099ed5f8ad6bfe4fe9d0f

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+ let ul = document.querySelector('#list')
+ let divPanel = document.querySelector("#show-panel")
+ fetch('http://localhost:3000/books')
+ .then(res=>res.json())
+ .then(data=>{
+    
+     for(let obj of data){
+        let li = document.createElement('li')
+        li.textContent = obj.title
+        ul.appendChild(li)
+
+   
+     }
+     
+
+ })
+
+
+});
